c4d1d0c4648925f58755102ef7a9bbaadee858e6
Mide Ibrahim <mide@Mides-MacBook-Pro.local>
2024-06-13 19:30:08 +0100

    ---

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,11 @@
-import heroImg from "../assets/heroImg.svg";
+import hero from "../assets/hero.png";
 import Button from "./Button";
 
 const Hero = () => {
   return (
     <>
       <section className="relative flex justify-center content lg:max-w-full">
-        <img className="object-cover" src={heroImg} alt="" />
+        <img className="object-cover" src={hero} alt="" />
         <Button
           buttonText="BUY COCO"
           className="hidden lg:block bg-[var(--clr-offwhite)] absolute text-black bottom-[10%] font-semibold"
